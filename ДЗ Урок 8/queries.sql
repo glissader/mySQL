83de5b474ad9c5d141b9be9ea9264d1e7f77bf00
glissader <75390256+glissader@users.noreply.github.com>
2021-09-26 15:00:18 +0300

    Выполненное ДЗ

--- ДЗ Урок 8/queries.sql
+++ ДЗ Урок 8/queries.sql
@@ -1,80 +1,35 @@
 use vk;
 
 # Пусть задан некоторый пользователь.
+set @user_id = 203;
 # Из всех друзей этого пользователя найдите человека, который больше всех общался с нашим пользователем
-set @user_id = 201;
-
-select count(*) top_friend_messages_count, user_id
-from (
-         select messages.id, messages.from_user_id as user_id
-         from messages
-         where (messages.from_user_id in (select initiator_user_id
-                                          from friend_requests
-                                          where target_user_id = @user_id
-                                            and status = 'approved'
-                                          union
-                                          select target_user_id
-                                          from friend_requests
-                                          where initiator_user_id = @user_id
-                                            and status = 'approved'))
-         union
-         select messages.id, messages.to_user_id as user_id
-         from messages
-         where (messages.to_user_id in (select initiator_user_id
-                                        from friend_requests
-                                        where target_user_id = @user_id
-                                          and status = 'approved'
-                                        union
-                                        select target_user_id
-                                        from friend_requests
-                                        where initiator_user_id = @user_id
-                                          and status = 'approved'))
-     ) as top
-group by user_id
-order by top_friend_messages_count desc
+select users.id, count(*)
+from users
+         join friend_requests fr
+              on (users.id = fr.initiator_user_id or users.id = fr.target_user_id) and status = 'approved'
+         join messages m on (users.id = m.from_user_id or users.id = m.to_user_id)
+where (initiator_user_id = @user_id or target_user_id = @user_id)
+  and users.id <> @user_id
+group by users.id
+order by 2 desc
 limit 1;
 
-# Подсчитать общее количество лайков, которые получили 10 самых молодых пользователей.
+# Подсчитать общее количество лайков, которые получили пользователи младше 10 лет
 select count(*)
-from (
-         select user_id,
-                (select TIMESTAMPDIFF(YEAR, profiles.birthday, now())
-                 from profiles
-                 where profiles.user_id = likes.user_id) as age
-         from likes
-         where media_id in (select id
-                            from media
-                            where user_id in (
-                                select user_id
-                                from profiles
-                                order by TIMESTAMPDIFF(YEAR, profiles.birthday, now())
-                            ))
-         order by age
-         limit 10
-     ) as top10;
+from users
+         join media m on m.user_id = users.id
+         join likes l on l.media_id = m.id
+         join profiles p on users.id = p.user_id
+where TIMESTAMPDIFF(YEAR, p.birthday, now()) < 10;
 
 # Определить кто больше поставил лайков (всего) - мужчины или женщины?
-select count(gender),
-       (
+select (
            case (gender)
                when 'f' then 'female'
                when 'm' then 'male'
                end
-           ) as gender
+           ) as gender,
+       count(*)
 from profiles
-where user_id in (select user_id from likes)
+         join likes l on profiles.user_id = l.user_id
 group by gender;
-
-# Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети.
-select *
-from users
-where id in (
-    select from_user_id
-    from (
-             select count(*), from_user_id
-             from messages
-             group by from_user_id
-             order by 1
-             limit 10
-         ) as loosers10
-);
